d1bb80a0962c5b6d4d38a46577a33a02a0dc1d60
Nikolai Hiemer <61734782+Googolpl3x@users.noreply.github.com>
2021-05-06 11:23:56 +0200

    Add files via upload

---
+++ SCHLANGE.java
@@ -0,0 +1,37 @@
+class SCHLANGE
+{
+    /** 
+     * Attributsliste
+     */
+    
+    
+    /**
+     * Konstruktor
+     * 
+     * Legt die Schlange an und stellt Anfangsposition und -bewgungsrichtung ein.
+     * @param x: x-Koordinate der Startposition des Schlangenkopfs
+     * @param y: y-Koordinate der Startposition des Schlangenkopfs
+     * @param richtung: Startrichtung des Schlangenkopfs
+     */
+    SCHLANGE (int x, int y, char richtung)
+    {
+
+    }
+    
+    /**
+     * Bewegt die Schlange ein Feld in der eingestellten Richtung.
+     */
+    void Bewegen ()
+    {
+        
+    }
+    
+    /**
+     * Setzt die Bewegunsrichtung der Schlange.
+     * @param richtungNeu (neue) Bewegungsrichtung
+     */
+    void RichtungSetzen (char richtungNeu)
+    {
+        
+    }
+}

bad743fb3cdfc0b44b1b3d282db0b178ce370dae
AFriendlyNecromancer <benediktostertag2@gmail.com>
2021-05-06 14:45:17 +0200

    Schach Projekt Version 0

--- Schachbrett.java
+++ Schachbrett.java
@@ -1,19 +1,19 @@
 
 /**
- * Write a description of class test1 here.
+ * Write a description of class Schachbrett here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class test1
+public class Schachbrett
 {
     // instance variables - replace the example below with your own
     private int x;
 
     /**
-     * Constructor for objects of class test1
+     * Constructor for objects of class Schachbrett
      */
-    public test1()
+    public Schachbrett()
     {
         // initialise instance variables
         x = 0;

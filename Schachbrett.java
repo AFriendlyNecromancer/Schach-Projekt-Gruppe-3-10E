396bca16ae77e0733ff79e5d60615727e30106ff
AFriendlyNecromancer <benediktostertag2@gmail.com>
2021-05-20 11:11:04 +0200

    Schach Projekt Version 1 -setup for programming

--- Schachbrett.java
+++ Schachbrett.java
@@ -6,28 +6,10 @@
  * @version (a version number or a date)
  */
 public class Schachbrett
+
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Schachbrett
-     */
-    public Schachbrett()
+     public Schachbrett()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+       
     }
 }

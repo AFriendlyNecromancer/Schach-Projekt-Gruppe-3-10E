fb9976809fee1043727b7992878ccff2337ba1bc
Nico2ops <nicovhomeyer@gmail.com>
2021-06-17 11:18:56 +0200

    Für mario

--- Schachfigur.java
+++ Schachfigur.java
@@ -4,14 +4,22 @@ public abstract class Schachfigur
     // instance variables - replace the example below with your own
     protected int x; //Spalte 0...7
     protected int y; // Zeile 0...7
-    protected bool IstWeiß;
+    protected boolean istWeiß;
+    protected char form;
+    
     //Annahme: xZiel und yZiel sind zwischen 0 und 7
     //Annahme: Zielpunkt != Startpunkt
     //Annahme: Niemand steht im Weg
     //Rückgabe: ist der zug erlaubt
-    public abstract bool BewegeNach(int xZiel, int yZiel); 
-    public @override bool BewegeNach(int xZiel, int yZiel);
+    public abstract boolean BewegeNach(int xZiel, int yZiel); 
+    
+    public Schachfigur(int x, int y, boolean istWeiß, char form)
     {
-     return xZiel == x || yZiel == y;
-    }    
+        this.x = x;
+        this.y = y;
+        this.istWeiß = istWeiß;
+        this.form = form;
+    }
+    }
+  
 }
--- Turm.java
+++ Turm.java
@@ -1,7 +1,31 @@
 public class Turm extends Schachfigur
 {
-    public Turm()
+    public Turm(int x, int y, boolean istWeiß)
     {
         
+        if(istWeiß)
+        {
+            form = '♖';
+        }
+        else
+        {
+            form = '♜';
+        }    
     }
-}
+    
+        
+     
+    
+        
+    
+    //char schwarzeFigur = '♜';
+    //char weißeFigur = '♖';
+    
+    public @override boolean BewegeNach(int xZiel, int yZiel)
+    {
+         return xZiel == x || yZiel == y;
+         }  
+   
+        
+    }
+

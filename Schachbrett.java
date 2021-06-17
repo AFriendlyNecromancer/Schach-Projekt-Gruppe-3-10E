8353d265dde7e6dba6e9dae0a6e632a0fe244fb7
Miogenes69 <85672787+Miogenes69@users.noreply.github.com>
2021-06-17 11:21:27 +0200

    Update Schachbrett.java

--- Schachbrett.java
+++ Schachbrett.java
@@ -1,8 +1,26 @@
-public class Schachbrett
-
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Button.*;
+class Schachbrett
 {
-     public Schachbrett()
+    Frame fenster;
+    
+    
+    /**
+     * Constructor for objects of class OBERFLAECHE
+     */
+    public Schachbrett()
     {
-       
-    }
+        fenster = new Frame("Schachbrett");
+        fenster.setSize(1080,1080);
+        fenster.setLocation(450,0);
+        fenster.setVisible(true);
+        fenster.setLayout(null);
+        
+         
+     
+        
 }
+
+}
+

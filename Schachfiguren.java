5e8d55825574ec786f02ca8256b4032e695c343b
Benedikt <benediktostertag2@gmail.com>
2021-07-08 11:22:04 +0200

    lole

--- Schachfiguren.java
+++ Schachfiguren.java
@@ -3,4 +3,24 @@ public abstract class Schachfiguren
 {
     int xJetzt;
     int yJetzt;
+    boolean CheckObFigurDaIst()
+    {
+        if (xAusgewähltesFeld == xAnfangsFeld)
+        {
+             
+        }
+        else
+        {
+          FigurIstNichtDa();
+        }
+        return true;
+    }
+    void FigurIstNichtDa()
+    {
+        System. out. println = "figur ist Nicht da!";
+    }
+    void WelcheFigur()
+    {
+        
+    }
 }
